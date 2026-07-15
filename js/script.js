84853356a6e5e43c033a289a604f4a5ddbb96275
Peter <101456132+Peter-developer01@users.noreply.github.com>
2026-07-15 21:34:56 +0300

    adapt the site down to about 330px

--- js/script.js
+++ js/script.js
@@ -1,8 +1,20 @@
+const mobileNavbar = document.querySelector(".navbar_mobile");
+let mobileNavbarOpen = false;
+
 document.addEventListener("click", (event) => {
+	let hamburger;
 	if (event.target.closest(".accordion__trigger")) {
 		const accordion = event.target.closest(".accordion");
 		accordion.classList.toggle("accordion_active");
 		const content = accordion.querySelector(".accordion__content");
 		content.style.maxHeight = content.style.maxHeight ? null : `${content.scrollHeight}px`;
+	} else if ((hamburger = event.target.closest(".header__hamburger"))) {
+		mobileNavbar.classList.toggle("navbar_mobile_active");
+		hamburger.classList.toggle("header__hamburger_active");
+		mobileNavbarOpen = !mobileNavbarOpen;
+	} else if ((!event.target.closest(".navbar_mobile") || event.target.closest(".navbar__menu-link")) && mobileNavbarOpen) {
+		mobileNavbar.classList.remove("navbar_mobile_active");
+		document.querySelector(".header__hamburger").classList.remove("header__hamburger_active");
+		mobileNavbarOpen = false;
 	}
 });
